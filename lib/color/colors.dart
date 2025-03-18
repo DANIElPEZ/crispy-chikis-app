2169e1291b6c436faa52fc11aa19125d01f57e56
Daniel Gonzalez <daniel.gonzalezubaque@gmail.com>
2025-03-17 21:56:48 -0500

    setup files

---
+++ lib/color/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+final Map<String, Color> colorsPalete={
+  'dark blue':Color(0xff147c88),
+  'dark brown':Color(0xff24151a),
+  'light brown':Color(0xff7d4c20),
+  'light blue':Color(0xff8dc7be),
+  'light green':Color(0xff9ce78e),
+  'pink':Color(0xffd9bba0),
+  'orange':Color(0xfff38e35),
+  'white':Color(0xffffffff)
+};
